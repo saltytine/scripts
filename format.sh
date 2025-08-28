1f60c2909fc805ef222e2677fffa4471ae3479a8
Saltine <156854448+saltytine@users.noreply.github.com>
2025-08-27 23:13:39 -0400

    Update format.sh

--- format.sh
+++ format.sh
@@ -2,9 +2,8 @@
 
 tmux \; \
   split-window -h -l 80 -- 'btop' \; \
-  split-window -v -l 20 -- 'kew' \; \
   select-pane -L \; \
   split-window -v -l 20 -- 'ranger' \; \
   select-pane -U \; \
-  attach
+  rename-window top
 
